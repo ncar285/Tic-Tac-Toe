c7e939ba7778ab058e8899bcc1cbdd1cbed45382
ncar285 <carlier.nicholas@gmail.com>
2023-10-04 11:57:14 -0700

    revert

--- src/index.js
+++ src/index.js
@@ -43,9 +43,10 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
-
-  document.getElementById('code-generate').addEventListener('click', generateCode)
-
+  document.getElementById('code-generate').addEventListener('click', ()=> {
+    const codeInput = document.getElementById('game-code')
+    codeInput.value = uuidv4().substr(0, 8).toUpperCase();;
+  })
 
   document.getElementById('copy-text').addEventListener('click', copyText);
 
