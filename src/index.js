2dc2ff051e03b35dc8b56f159f4076ac63b1874a
ncar285 <carlier.nicholas@gmail.com>
2023-10-03 17:48:50 -0700

    copies to the user's clipboard

--- src/index.js
+++ src/index.js
@@ -4,19 +4,40 @@ import { v4 as uuidv4 } from 'uuid';
 
 document.addEventListener("DOMContentLoaded", () => {
 
+  const copyText = () => {
+    const copyIcon = document.getElementById('copy-text');
+    const copyMessage = document.getElementById('copy-message');
+    const code = document.getElementById('game-code').value
+    setCopyState(copyIcon, copyMessage);
+    copyToClipboard(code);
+    setTimeout(() => resetState(copyIcon, copyMessage), 1000);
+  }
+
+  const setCopyState = (icon, message) => {
+    icon.src = "assets/tick.svg";
+    message.style.display = "block";
+  }
+
+  const resetState = (icon, message)  => {
+      icon.src = "assets/copy-text.svg";
+      message.style.display = "none";
+  }
+
+  const copyToClipboard = async (text) => {
+    try {
+        await navigator.clipboard.writeText(text);
+    } catch (err) {
+        console.error('Failed to copy text: ', err);
+    }
+  }
+
   document.getElementById('code-generate').addEventListener('click', ()=> {
     const codeInput = document.getElementById('game-code')
     codeInput.value = uuidv4().substr(0, 8);
   })
 
-  document.getElementById('copy-text').addEventListener('click',() => {
-    document.getElementById('copy-text').src = "assets/tick.svg"
-    document.getElementById('copy-message').style.display = "block"
-    setTimeout(()=>{
-      document.getElementById('copy-text').src = "assets/copy-text.svg"
-      document.getElementById('copy-message').style.display = "none"
-    }, 1000)
-  })
+  document.getElementById('copy-text').addEventListener('click', copyText);
+
 
   const game = new Game();
   const ele = document.querySelector(".ttt")
